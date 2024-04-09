96ddfe697a67f55ddbe5098ac1d0928efa30ce5e
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-04-09 14:07:07 -0500

    Now we can add employee

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -39,5 +41,13 @@ public class EmployeeController {
         theModel.addAttribute("employee", theEmployee);
 
         return "/employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+        employeeService.save(theEmployee);
+
+        return "redirect:/employees/list";
     }
 }
